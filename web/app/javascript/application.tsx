cafb400d96f4e696a556d56952b4d11c0e72e970
Naoki0722 <mattu.nao722@gmail.com>
2022-12-18 01:02:34 +0900

    fix

--- web/app/javascript/application.tsx
+++ web/app/javascript/application.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-
 function App() {
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
-};
+  return <h1>Hello World</h1>;
+}
 
 const root = document.querySelector('#root');
 if (!root) {
   throw new Error('No root element');
 }
-
-
 createRoot(root).render(
   <React.StrictMode>
     <App />
