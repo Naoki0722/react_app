4b0b2db86b2c9d16bf05ada5735a67d5869d77f0
Naoki0722 <mattu.nao722@gmail.com>
2022-12-25 09:57:31 +0900

    add mutation

--- web/app/graphql/mutations/create_user_mutation.rb
+++ web/app/graphql/mutations/create_user_mutation.rb
@@ -1,5 +1,28 @@
 module Mutations
   class CreateUserMutation < BaseMutation
+    argument :name, String
+    argument :age, Integer
+    argument :memo, String
+
+    field :user, Types::React::UserType
+
+    def resolve(name:, age:, memo:)
+      begin
+        user = User.create({name:, age:, memo:})
+        return { user: }
+      rescue => exception
+        raise GraphQL::ExecutionError, "user create failed"
+      end
+    end
+
+    # def resolve(**args)
+    #   user = User.create!(args)
+    #   {
+    #     user: user
+    #   }
+    # end
+
+
     # TODO: define return fields
     # field :post, Types::PostType, null: false
 
