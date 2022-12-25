98a786db5c281e1a8f9fbf0e0ac1c51382d04a02
Naoki0722 <mattu.nao722@gmail.com>
2022-12-25 10:29:20 +0900

    add update mutation

--- web/app/graphql/types/mutation_type.rb
+++ web/app/graphql/types/mutation_type.rb
@@ -1,11 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :update_user_mutation, mutation: Mutations::UpdateUserMutation
     field :create_user_mutation, mutation: Mutations::CreateUserMutation
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
   end
 end
